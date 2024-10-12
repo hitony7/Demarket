861671b6b7143d2b4b6559f9152ef454b700afde
Tony wong <tywong11@gmail.com>
2024-10-11 23:13:36 -0600

    remove boilerplate

--- frontend/Demarket/src/app/app.component.ts
+++ frontend/Demarket/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MainpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

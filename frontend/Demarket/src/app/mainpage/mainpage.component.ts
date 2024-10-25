7e46cb4b5a0e6a92c852d70c095bd57c0bdace5e
Tony wong <tywong11@gmail.com>
2024-10-24 21:37:02 -0600

    footer imports

--- frontend/Demarket/src/app/mainpage/mainpage.component.ts
+++ frontend/Demarket/src/app/mainpage/mainpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-mainpage',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })

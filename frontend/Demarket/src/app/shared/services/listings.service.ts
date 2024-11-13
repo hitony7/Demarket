d074eacf6364836778adccc53e77d9272415157b
Tony wong <tywong11@gmail.com>
2024-11-12 18:17:21 -0700

    listing service for api request

---
+++ frontend/Demarket/src/app/shared/services/listings.service.ts
@@ -0,0 +1,22 @@
+// services/listing.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environment/environment'; // Import the environment to manage the API URL
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ListingService {
+  private apiUrl = `${environment.apiBaseUrl}/api/listing`;
+
+  constructor(private http: HttpClient) {}
+
+  // Create a listing
+  createListing(formData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${localStorage.getItem('token')}`, // Attach JWT token for authorization
+    });
+    return this.http.post(`${this.apiUrl}`, formData, { headers });
+  }
+}

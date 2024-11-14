2fe6b052e368b180cec359408c295f22e280f94b
Tony wong <tywong11@gmail.com>
2024-11-14 15:06:18 -0700

    add rep field to user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema(
     nonce: { type: String },
     bio: { type: String },
     links: { type: String },
+    rep: { type: Number, default: 0}
   },
   { timestamps: true, collection: 'users' } // Adds explicit collection name
 );

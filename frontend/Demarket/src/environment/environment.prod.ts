e1705952f11bf0bf403759399a5839347480b20a
Tony wong <tywong11@gmail.com>
2024-11-24 18:15:02 -0700

    "Remove environment files from version control"

--- frontend/Demarket/src/environment/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl: 'https://your-production-backend.com', // Replace with your production backend API URL
-  };
-  
--- frontend/Demarket/src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiBaseUrl: 'http://localhost:3000', // Replace with your backend API URL
-  };
-  
